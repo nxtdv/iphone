66d43800c8edb512252ca7719bed274502898667
nxtdv <frags31560@gmail.com>
2022-09-15 14:59:33 +0200

    🍻 Add declaration

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -6,4 +6,7 @@ declare module "*.png";
 declare module "*.jpg";
 declare module "*.jpeg";
 declare module "*.gif";
-declare module "*.svg";
+declare module "*.svg" {
+  const content: any;
+  export default content;
+}
