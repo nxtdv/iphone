26e2a436a0d146cb032162802fb5f152132f6522
nxtdv <frags31560@gmail.com>
2022-09-15 15:03:03 +0200

    📦 Remove deploy command

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,3 +1,4 @@
+/// <reference types="react-scripts" />
 declare module "*.ttf";
 declare module "*.otf";
 declare module "*.woff";
@@ -7,6 +8,6 @@ declare module "*.jpg";
 declare module "*.jpeg";
 declare module "*.gif";
 declare module "*.svg" {
-  const content: any;
+  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
   export default content;
 }
