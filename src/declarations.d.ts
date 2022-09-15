a7d5fbff1e62cdd889480c07c3f66dc06bdedcd5
nxtdv <frags31560@gmail.com>
2022-09-15 15:06:03 +0200

    ✅ Update declaration

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -7,7 +7,4 @@ declare module "*.png";
 declare module "*.jpg";
 declare module "*.jpeg";
 declare module "*.gif";
-declare module "*.svg" {
-  const content: any;
-  export default content;
-}
+declare module "*.svg";
