557ccdc8d415a4bc2125e595fdf583ed5e4b650e
nxtdv <frags31560@gmail.com>
2022-09-15 10:20:10 +0200

    🌱 Change to d.ts

---
+++ src/declarations.d.ts
@@ -0,0 +1,9 @@
+declare module "*.ttf";
+declare module "*.otf";
+declare module "*.woff";
+declare module "*.woff2";
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.gif";
+declare module "*.svg";
