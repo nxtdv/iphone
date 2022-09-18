19e26e84f4eb45f81408b4daca3b53aa788115ba
nxtdv <frags31560@gmail.com>
2022-09-18 11:56:51 +0200

    ⚰️ Remove lockscreen code

--- src/pages/Phone.tsx
+++ src/pages/Phone.tsx
@@ -1,11 +1,8 @@
-import { Suspense, useState } from "react";
+import { Suspense } from "react";
 import styles from "./phone.module.scss";
-import HeaderBar from "./HeaderBar";
-import CurrentTime from "../components/times/CurrentTime";
-import CurrentDate from "../components/date/CurrentDate";
+import { LockScreen } from "./LockScreen";
 
 function Phone() {
-  const [locked, setPhoneLocked] = useState(true);
   const loading = () => <div className={styles.loading}>Chargement...</div>;
 
   return (
@@ -14,21 +11,9 @@ function Phone() {
 
       <Suspense fallback={loading()}>
         <div className={styles.phoneContent}>
-          <HeaderBar lock={locked} />
-          {locked ? (
-            <div className={styles.locked}>
-              <CurrentDate />
-              <CurrentTime
-                fontSize="70px"
-                fontFamily="SF-Pro-Rounded-Medium"
-                lineHeight="0"
-                letterSpacing="-2.12px"
-                height="62px"
-              />
-            </div>
-          ) : (
-            <div className={styles.locked}>Unlocked</div>
-          )}
+          <LockScreen>
+            <p>yo le rap</p>
+          </LockScreen>
         </div>
       </Suspense>
     </div>
