0208b6cfe1070987c0a68e0f2c0b9160e014856d
nxtdv <frags31560@gmail.com>
2022-09-21 16:46:51 +0200

    ✨ Add apps in home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,32 @@
+import { useMemo } from "react";
+import { useNavigate } from "react-router-dom";
+import appsRoutes from "../Apps/appsRoutes";
 import styles from "./Home.module.scss";
+import AppIcon from "../../components/AppIcon/AppIcon";
 import HeaderBar from "../../components/HeaderBar/HeaderBar";
 
 function Home() {
+  const navigate = useNavigate();
+  const functioningApps = useMemo(() => {
+    return appsRoutes.map((app) => {
+      const onClick = () => navigate(app.destination);
+
+      return (
+        <AppIcon
+          key={app.id}
+          title={app.title}
+          iconPath={app.iconPath}
+          onClick={onClick}
+        />
+      );
+    });
+  }, [navigate]);
+
   return (
     <>
       <HeaderBar lock={false} />
-      <div className={styles.container}>
-        <p>children</p>
+      <div className={styles.home}>
+        <div className={styles.page}>{functioningApps}</div>
       </div>
     </>
   );
