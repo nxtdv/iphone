906c1db20c42fb537595a180b67f5929e648b5ff
nxtdv <nxtdv@users.noreply.github.com>
2022-09-21 14:49:15 +0000

    deploy: 8158c19e5473ad8760cac2195016718c7e80f405

---
+++ assets/Calculator.b072fe44.js
@@ -0,0 +1,1 @@
+import{a}from"./index.629a497e.js";function t(){return a("div",{children:"Calculator"})}export{t as default};
---
+++ assets/Notes.7bdf24df.js
@@ -0,0 +1,1 @@
+import{a as t}from"./index.629a497e.js";function o(){return t("div",{children:"Notes"})}export{o as default};
