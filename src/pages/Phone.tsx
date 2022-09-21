8158c19e5473ad8760cac2195016718c7e80f405
nxtdv <frags31560@gmail.com>
2022-09-21 16:47:45 +0200

    ✨ Adding routes

--- src/pages/Phone.tsx
+++ src/pages/Phone.tsx
@@ -1,10 +1,19 @@
-import { Suspense } from "react";
+import { Suspense, lazy, useMemo } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import styles from "./phone.module.scss";
 import { LockScreen } from "./LockScreen/LockScreen";
-import Home from "./Home/Home";
+import appsRoutes from "./Apps/appsRoutes";
+
+const Home = lazy(() => import("./Home/Home"));
 
 function Phone() {
   const loading = () => <div className={styles.loading}>Chargement...</div>;
+  const routes = useMemo(() => {
+    return appsRoutes.map((app) => {
+      const Element = app.element;
+      return <Route key={app.id} path={app.route} element={<Element />} />;
+    });
+  }, []);
 
   return (
     <div className={styles.phoneContainer}>
@@ -13,7 +22,12 @@ function Phone() {
       <Suspense fallback={loading()}>
         <div className={styles.phoneContent}>
           <LockScreen>
-            <Home />
+            <BrowserRouter basename="/iphone">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                {routes}
+              </Routes>
+            </BrowserRouter>
           </LockScreen>
         </div>
       </Suspense>
