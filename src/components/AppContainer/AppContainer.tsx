f67d1ef926cd0a3104c70f4833eb9b466799d722
nxtdv <frags31560@gmail.com>
2022-09-22 20:25:56 +0200

    ✨ Init AppContainer

---
+++ src/components/AppContainer/AppContainer.tsx
@@ -0,0 +1,26 @@
+import { PropsWithChildren } from "react";
+import HeaderBar from "../HeaderBar/HeaderBar";
+import HomeBar from "../HomeBar/HomeBar";
+
+type Props = {
+  isDarkMode?: boolean;
+  showStatusBar?: boolean;
+  showHomeButton?: boolean;
+};
+
+function AppContainer({
+  children,
+  showStatusBar = true,
+  isDarkMode,
+  showHomeButton = true,
+}: PropsWithChildren<Props>) {
+  return (
+    <>
+      {showStatusBar && <HeaderBar lock={false} isDarkMode={isDarkMode} />}
+      {children}
+      {showHomeButton && <HomeBar isDarkMode={isDarkMode} />}
+    </>
+  );
+}
+
+export default AppContainer;
