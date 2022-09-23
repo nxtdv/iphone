4010b9222071ef2188b34bf12c03dc0296209168
nxtdv <frags31560@gmail.com>
2022-09-23 22:28:25 +0200

    ✨ Init Calculator app

--- src/pages/Apps/Calculator/Calculator.tsx
+++ src/pages/Apps/Calculator/Calculator.tsx
@@ -1,5 +1,13 @@
-function Calculator() {
-  return <div>Calculator</div>;
-}
+import { useState, useEffect } from "react";
+import styles from "./Calculator.module.scss";
+import AppContainer from "../../../components/AppContainer/AppContainer";
+
+const Calculator = () => {
+  return (
+    <AppContainer>
+      <div className={styles.calculatorContainer}>Calculator</div>
+    </AppContainer>
+  );
+};
 
 export default Calculator;
