8dcee6d91da6c1cc6cde4517059b4bb0a148f861
nxtdv <frags31560@gmail.com>
2022-09-23 22:28:09 +0200

    ✨ Init Notes app

--- src/pages/Apps/Notes/Notes.tsx
+++ src/pages/Apps/Notes/Notes.tsx
@@ -1,7 +1,12 @@
 import AppContainer from "../../../components/AppContainer/AppContainer";
+import styles from "./Notes.module.scss";
 
 function Notes() {
-  return <AppContainer isDarkMode>Notes</AppContainer>;
+  return (
+    <AppContainer isDarkMode>
+      <div className={styles.notesContainer}>Notes</div>
+    </AppContainer>
+  );
 }
 
 export default Notes;
