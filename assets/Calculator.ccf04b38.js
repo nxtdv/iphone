bceca926641a08f81ee5851f3ab0d82fe7562e28
nxtdv <nxtdv@users.noreply.github.com>
2022-09-27 12:55:09 +0000

    deploy: 2bec079021f5f5d22613f7589ab1b0fa03f62aa0

---
+++ assets/Calculator.ccf04b38.js
@@ -0,0 +1,1 @@
+import{A as t}from"./AppContainer.75a045af.js";import{a}from"./index.16d6edef.js";const r="_calculatorContainer_1tw7i_1",o={calculatorContainer:r},n=()=>a(t,{children:a("div",{className:o.calculatorContainer,children:"Calculator"})});export{n as default};
