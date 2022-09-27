a11ffff7702031b6033a2687b809c6948b07c378
nxtdv <frags31560@gmail.com>
2022-09-27 15:04:47 +0200

    ♻️ Refacto `HeaderBar`

--- src/components/HeaderBar/HeaderBar.tsx
+++ src/components/HeaderBar/HeaderBar.tsx
@@ -12,45 +12,32 @@ type Props = {
 
 const HeaderBar = ({ lock, isDarkMode }: Props) => {
   return (
-    <>
+    <div
+      className={`${styles.phoneHeader} ${
+        isDarkMode ? styles.phoneHeaderDark : styles.phoneHeaderLight
+      }`}
+      style={{ position: lock ? "relative" : "absolute" }}
+    >
       {lock ? (
-        <div
-          className={`${styles.phoneHeader} ${
-            isDarkMode ? styles.phoneHeaderDark : styles.phoneHeaderLight
-          }`}
-        >
-          <div className={styles.phoneHeaderLeft} />
-          <div className={styles.phoneHeaderRight}>
-            <ReactSVG className={styles.phoneHeaderIcon} src={mobilesignal} />
-            <ReactSVG className={styles.phoneHeaderIcon} src={wifi} />
-            <ReactSVG className={styles.phoneHeaderIcon} src={batterie} />
-          </div>
-        </div>
+        <div className={styles.phoneHeaderLeft} />
       ) : (
-        <div
-          className={`${styles.phoneHeader} ${
-            isDarkMode ? styles.phoneHeaderDark : styles.phoneHeaderLight
-          }`}
-          style={{ position: "absolute" }}
-        >
-          <div className={styles.phoneHeaderLeft}>
-            <CurrentTime
-              fontSize="10.5px"
-              fontFamily="SF-Pro-Text-Regular"
-              lineHeight="22px"
-              letterSpacing="-0.41px"
-              color="#fff"
-              isDarkMode={isDarkMode}
-            />
-          </div>
-          <div className={styles.phoneHeaderRight}>
-            <ReactSVG className={styles.phoneHeaderIcon} src={mobilesignal} />
-            <ReactSVG className={styles.phoneHeaderIcon} src={wifi} />
-            <ReactSVG className={styles.phoneHeaderIcon} src={batterie} />
-          </div>
+        <div className={styles.phoneHeaderLeft}>
+          <CurrentTime
+            fontSize="10.5px"
+            fontFamily="SF-Pro-Text-Regular"
+            lineHeight="22px"
+            letterSpacing="-0.41px"
+            color="#fff"
+            isDarkMode={isDarkMode}
+          />
         </div>
       )}
-    </>
+      <div className={styles.phoneHeaderRight}>
+        <ReactSVG className={styles.phoneHeaderIcon} src={mobilesignal} />
+        <ReactSVG className={styles.phoneHeaderIcon} src={wifi} />
+        <ReactSVG className={styles.phoneHeaderIcon} src={batterie} />
+      </div>
+    </div>
   );
 };
 
