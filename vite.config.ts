ef098c58b5390e7848df64289a99fa3197d491a1
nxtdv <frags31560@gmail.com>
2022-12-02 15:25:27 +0100

    🔧 Adding terser for build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/iphone/",
+  base: "./",
+  build: {
+    minify: "terser",
+  },
   publicDir: "public",
   plugins: [react()],
 });
